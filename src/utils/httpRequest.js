bb56487cc3bcbbf99ab1a7df8638c98fe5aee201
duy1111 <maixuanduy0605@gmail.com>
2022-09-20 14:14:19 +0700

    UI-Sidebar #2

--- src/utils/httpRequest.js
+++ src/utils/httpRequest.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 console.log(process.env.REACT_APP_BASE_URL);
 const httpRequest = axios.create({
-    baseURL: 'process.env.REACT_APP_BASE_URL',
+    baseURL: process.env.REACT_APP_BASE_URL,
 });
 export const get = async (path, options = {}) => {
     const response = await httpRequest.get(path, options);
